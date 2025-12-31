9678e9baa099921cb0c20036e0fee61503723b5e
tgkim <je23ct@gmail.com>
2025-12-31 16:40:07 +0900

    fix build error

--- projects/online-editor/src/app/layout.tsx
+++ projects/online-editor/src/app/layout.tsx
@@ -109,7 +109,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link 
-          href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;600&display=swap" 
+          href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;600&amp;display=swap" 
           rel="stylesheet"
           as="style"
         />
